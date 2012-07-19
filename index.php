10c67ecdf67d5fdb977e46a4b70dc7f1772fe451
Conor Haining <conor.haining@gmail.com>
2012-07-19 16:53:52 +0100

    Basic stuff
    Basic layout of the page and including some css files

--- index.php
+++ index.php
@@ -2,5 +2,21 @@
 <html>
 	<head>
 		<title>YourBucket</title>
+
+		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="css/global.css">
 	</head>
+	<body>
+		<div id="fb-root"></div>
+		<div id="wrapper">
+			<header>
+				<a href="/" title="Return to the homepage" id="logo">
+					<img src="/images/logo.png" alt="YourBucket Logo" />
+				</a>
+				<div class="login">
+					<a href="./connect" title="Connect to Facebook">Connect to Facebook</a>
+				</div>
+			</header>
+		</div>
+	</body>
 </html>
