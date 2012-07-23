c26a815cf17f5197a19e03ba153d3f04f1a55e4c
Conor Haining <conor.haining@gmail.com>
2012-07-23 20:58:44 +0100

    Refinements

--- dashboard/index.php
+++ dashboard/index.php
@@ -32,12 +32,9 @@ function findfriend($friends, $t){
 	}
 }
 
-// Easier Var
+// Easier Var & Cookie set (For bucket.js)
 $id = $_SESSION['fb_id'];
-
-// Get users info from DB
-$user_db = mysql_query("SELECT * FROM user WHERE fb_id='".$id."'");
-$user_db = mysql_fetch_assoc($user_db);
+setcookie("id", $id);
 
 // Get Users Buckets
 $filename = "data/".$id.".json";
@@ -48,6 +45,7 @@ fclose($handle);
 $json = json_decode($contents, true);
 
 $i = 1;
+
 // Get user's friends
 $friends = file_get_contents("https://graph.facebook.com/".$id."/friends?".$user_db['token']);
 $friends = json_decode($friends, true);
@@ -61,30 +59,40 @@ $friends = json_decode($friends, true);
 	<link rel="stylesheet" type="css/text" href="../css/normalize.css">
 	<link rel="stylesheet" type="css/text" href="../css/global.css">
 	<link rel="stylesheet" type="css/text" href="../css/dashboard.css">
+	<link rel="stylesheet" type="css/text" href="../css/jquery-ui-1.8.21.custom.css">
 </head>
 <body>
 	<div class="overlay"></div>
 	<div class="add-bucket-popup">
 		<h3>Add New Bucket</h3>
 
-		<form method="post" action="">
-			<label for="title">Bucket Name: </label>
-			<input type="text" placeholder="Skydiving">
-			<label for="title">Who with?: </label>
-			<input class="datalist-input" type="text" list="fb_friends">
-			<datalist id="fb_friends">
-				<?php foreach($friends['data'] as $f) : ?>
-					<option data-fb_id="<?php echo $f['id']; ?>" value="<?php echo $f['name']; ?>"></option>
-				<?php endforeach; ?>
-			</datalist>
-			<label for="title">Before I'm: </label>
-			<input type="number" min="13" step="1">
+		<form method="post" action="bucket.php">
+			<div class="group">
+				<label for="title">Bucket Name: </label>
+				<input type="text" name="name" placeholder="Skydiving">
+			</div>
+			<div class="fb_friends group">
+				<label for="title">Who with?: </label>
+				<select name="friend-1" id="fb_friends">
+					<?php foreach($friends['data'] as $f) : ?>
+						<option value="<?php echo $f['id']; ?>"><?php echo $f['name']; ?></option>
+					<?php endforeach; ?>
+				</select>
+				<a href="#" class="add-friend" title="More friends">+</a>
+			</div>
+			<div class="group">
+				<label for="title">Before I'm: </label>
+				<input type="number" name="beforeiam" min="13" step="1">
+			</div>
+			<div class="group">
+				<input type="submit" name="submit" value="Add Bucket"/>
+			</div>
 		</form>
 	</div>
 	<div id="wrapper">
 		<header class="small">
 			<a href="" id="logo">
-				<img src="/images/logo.png" alt="YourBucket Logo" />
+				<img src="../img/logo-small.png" alt="YourBucket Logo" />
 			</a>
 		</header>
 		<aside class="sidebar">
@@ -99,6 +107,7 @@ $friends = json_decode($friends, true);
 					<td>#</td>
 					<td>Bucket</td>
 					<td>With Who?</td>
+					<td>Before I am</td>
 					<td>How's it going?<td>
 				</thead>
 				<tbody>
@@ -107,6 +116,7 @@ $friends = json_decode($friends, true);
 							<td><?php echo $i; ?></td>
 							<td><?php echo $json[$i]['title']; ?></td>
 							<td><?php findfriend($json[$i]['with'], $user_db['total']); ?></td>
+							<td><?php echo $json[$i]['before']; ?></td>
 							<td><?php echo $json[$i]['status']; ?></td>
 						</tr>
 					<?php $i++; endwhile; ?>
@@ -118,7 +128,7 @@ $friends = json_decode($friends, true);
 		</footer>
 	</div>
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	<script src="../js/modernizr.custom.95508.js"></script>
+	<script type="text/javascript" src="../js/modernizr.js"></script>
 	<script type="text/javascript" src="../js/bucket.js"></script>
 </body>
 </html>
