fc9736385697823176207fa6e54d119f76d9e113
Conor Haining <conor.haining@gmail.com>
2012-07-23 20:59:11 +0100

    get users DB info from this file instead of rewriting it

--- database.php
+++ database.php
@@ -9,4 +9,8 @@ $dbDatabase = "yourbucket";       //Database Name
 $db = mysql_connect("$dbHost", "$dbUser", "$dbPass") or die ("Error connecting to database.");
 mysql_select_db("$dbDatabase", $db) or die ("Couldn't select the database.");
 
+// Get users info from DB
+$user_db = mysql_query("SELECT * FROM user WHERE fb_id='".$_SESSION['fb_id']."'");
+$user_db = mysql_fetch_assoc($user_db);
+
 ?>
